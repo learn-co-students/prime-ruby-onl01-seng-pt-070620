82564cdd08c778ad25e57e8ab4df019860a35b84
Andy Adams <andrewdcadams@gmail.com>
2020-07-10 15:23:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime?(num)
+  num = num.abs
+
+   n = 2
+  return false if num == 1 || num == 0 
+  while n < (num)
+    if num % n == 0
+
+      return false
+     end
+     n += 1
+  end
+
+  true
+end
+
+
+prime?(2)
