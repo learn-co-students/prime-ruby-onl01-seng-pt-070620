787511b8070e74e4f9fc7560c10165f7fcae35ed
Ebo Lee <yiboli.coding@gmail.com>
2020-07-13 04:22:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+     if num < 2
+      return false
+    else
+    (2..(num - 1)).each do |i|
+      return false if num % i == 0
+    end
+    true
+  end 
+end
