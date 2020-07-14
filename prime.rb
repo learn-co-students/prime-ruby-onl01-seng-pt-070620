ee5656b2630769847413ec3a39c72f10b438ce83
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-14 00:22:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+
+#1. Prime number is divisible ONLY by itself, and 1 (not divisible )
+#primes always have a remainde
+def prime?(number)
+  range = (2..(number - 1))
+  range = range.to_a #[2, 3, 4, 5]
+
+  if number < 0
+     return false
+  elsif number == 0 || number == 1
+    return false
+  end
+  
+  range.all? do |num|
+    # binding.pry
+    number % num != 0
+  end
+end
