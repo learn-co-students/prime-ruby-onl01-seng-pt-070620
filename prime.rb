6a4cd72064fd5145780322dbf41e75c62c997d8e
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-15 02:16:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+if number <= 1
+  return false
+ else
+   (2..(number - 1)).each do |n|
+     if number % n==0
+      return false
+    end
+   end
+  end
+   true
+end 
