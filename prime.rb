8d5ac39812b2b3c3444fa49183055a5a19fd6042
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-10-16 12:09:06 -0400

    improve iterator

--- prime.rb
+++ prime.rb
@@ -1,10 +1,19 @@
+# def prime?(number)
+#   if number <= 1
+#    false
+#   else
+#     (2..(number - 1)).each do |n|
+#       return false if number% n == 0
+#       end
+#       true
+#    end
+# end
+
+
 def prime?(number)
   if number <= 1
-   false
+    false
   else
-    (2..(number - 1)).each do |n|
-      return false if number% n == 0
-      end
-      true
-   end
+    (2..(number - 1)).all? {|i| number % i != 0}
+  end
 end
